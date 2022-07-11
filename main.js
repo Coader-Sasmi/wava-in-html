ff786c955c30cbf1bd5c98d7d969dbe70a614c05
Coader-Sasmi <thesasmita.mahanta@gmail.com>
2022-07-11 08:01:05 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+function myFunction() {
+    var a = document.getElementById("menu");
+    if (a.style.display == "block") {
+        a.style.display = "none";
+    }
+    else {
+        a.style.display = "block";
+    }
+
+}
